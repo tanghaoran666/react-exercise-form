72b0687de0631637e12448bae0ad7f2561e266f1
tanghaoran <407458281@qq.com>
2020-09-23 20:07:43 +0800

    [thr] implement basic function

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,14 +1,54 @@
-import React, {Component} from 'react';
-import './myProfile.less';
+import React, { Component } from "react";
+import "./myProfile.less";
 
 class MyProfile extends Component {
+  state = {
+    name: '',
+    gender: '',
+    descriptions: '',
+    isRead: false,
+  }
+  
+  handleChange= (event) => {
+    this.setState(
+      {[event.target.name] : event.target.value}
+      )
+  }
+  handleSubmit= (event) => {
+    console.log('name:',this.state.value);
+    console.log('gender:',this.state.gender)
+    console.log('descriptions:',this.state.descriptions)
+    console.log('isRead:',this.state.isRead)
+    event.preventDefault();
+  }
   render() {
     return (
-      <form></form>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" name="name" onChange={this.handleChange} value={this.state.name} />
+        </label>
+        <br />
+        <label>
+          Gender:
+          <select name="gender" value={this.state.gender} onChange={this.handleChange} >
+            <option value='male'>Male</option>
+            <option value='female'>Female</option>
+          </select>
+        </label>
+        <br />
+        <label>
+          <textarea name="descriptions" value={this.state.descriptions} onChange={this.handleChange}>Description yourself</textarea>
+        </label>
+        <br />
+        <label>
+          <input name="isRead" type='checkbox' checked={this.state.isRead} onChange={this.handleChange}/> I have read the term of conducts
+        </label>
+        <br />
+        <input type='submit' value='Submit' disabled={!this.state.name || !this.state.gender || !this.state.descriptions || !this.state.isRead} />
+      </form>
     );
   }
 }
 
 export default MyProfile;
-
-
